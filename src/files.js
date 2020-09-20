9a64a8559f7e339978abbce0564a0286029ccf37
sakistellina <69436794+sakistellina@users.noreply.github.com>
2020-09-20 23:51:30 +0200

    New

--- src/files.js
+++ src/files.js
@@ -51,9 +51,7 @@ function handleSubmit(event) {
 
 function searchLocation(position) {
   let apiKey = "8f2a727a8d5b5cadf3134d55a1087a44";
-  let apiUrl = `https://api.openweathermap.org/data/2.5/weather?lat=${
-    position.coords.latitude
-    }&lon=${position.coords.longitude}&appid=${apiKey}&units=metric`;
+  let apiUrl = `https://api.openweathermap.org/data/2.5/weather?lat=${position.coords.latitude}&lon=${position.coords.longitude}&appid=${apiKey}&units=metric`;
 
   axios.get(apiUrl).then(displayWeatherCondition);
 }
